092bda899cc09c47006b800d1e5594d8fbe32a29
Philip K. Oyelegbin <72349309+PhilipOyelegbin@users.noreply.github.com>
2022-07-18 09:51:43 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,10 @@ const App = () => {
       <Mainpage>
         <Hero/>
         <Carousel/>
+        <a href="https://www.konga.com/category/automotive-industrial-2051?utm_source=affiliates&utm_medium=web&utm_term=acquisition&utm_content=09_15_2020&utm_campaign=acquisition&amp;k_id=MoonShot&amp;k_bid=7db94b70" rel="nofollow" target="_top">
+          <img src="//konga.postaffiliatepro.com/accounts/default1/bo3dhla/7db94b70.jpg" alt="" title="" width="300" height="600" />
+        </a>
+        <img style="border:0" src="https://konga.postaffiliatepro.com/scripts/io3dhla?k_id=MoonShot&amp;k_bid=7db94b70" width="1" height="1" alt="" />
         <Products/>
       </Mainpage>
       <Footer/>
