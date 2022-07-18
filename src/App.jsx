790dbe3efcfd12758fe07b8ff6e30f3a1d68662a
Philip K. Oyelegbin <72349309+PhilipOyelegbin@users.noreply.github.com>
2022-07-18 09:05:48 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,11 @@ const App = () => {
       <Mainpage>
         <Hero/>
         <Carousel />
-        <div>
-          <a href="https://www.konga.com/category/automotive-industrial-2051?utm_source=affiliates&utm_medium=web&utm_term=acquisition&utm_content=09_15_2020&utm_campaign=acquisition&amp;k_id=MoonShot&amp;k_bid=125e451d" rel="nofollow" target="_top"><img src="//konga.postaffiliatepro.com/accounts/default1/bo3dhla/125e451d.jpg" alt="" title="" width="728" height="90" /></a><img style="border:0" src="https://konga.postaffiliatepro.com/scripts/io3dhla?k_id=MoonShot&amp;k_bid=125e451d" width="1" height="1" alt=""/>
+        <div className='px-3'>
+          <a href="https://www.konga.com/category/automotive-industrial-2051?utm_source=affiliates&utm_medium=web&utm_term=acquisition&utm_content=09_15_2020&utm_campaign=acquisition&amp;k_id=MoonShot&amp;k_bid=125e451d" rel="nofollow" target="_top">
+            <img src="//konga.postaffiliatepro.com/accounts/default1/bo3dhla/125e451d.jpg" alt="" title="" width="728" height="90" />
+          </a>
+          <img style="border:0" src="https://konga.postaffiliatepro.com/scripts/io3dhla?k_id=MoonShot&amp;k_bid=125e451d" width="1" height="1" alt=""/>
         </div>
         <Products/>
       </Mainpage>
